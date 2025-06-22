3f2449300a4dc3df27d63e84e8046450e5ecef94
Manas1117 <manasrathod2003@gmail.com>
2025-06-22 15:02:00 +0530

    Phone Friendly

--- script.js
+++ script.js
@@ -10,15 +10,42 @@ let h3 = document.querySelector("h3");
 
 let instructionsBox = document.getElementById("instructions");
 
-document.addEventListener("keypress", function () {
+// from here Phone functionality code
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+// ✅ Set startup message
+if (isMobile) {
+  h2.innerText = "Tap anywhere to Start";
+} else {
+  h2.innerText = "Press any key to Start";
+}
+
+function handleStart() {
   instructionsBox.style.display = "none";
-  if (started == false) {
+  if (!started) {
     console.log("Game started");
     started = true;
-
     levelUp();
   }
-});
+}
+
+// ✅ Setup event listeners
+if (isMobile) {
+  document.addEventListener("touchstart", handleStart);
+} else {
+  document.addEventListener("keypress", handleStart);
+}
+// till here
+
+// document.addEventListener("keypress", function () {
+//   instructionsBox.style.display = "none";
+//   if (started == false) {
+//     console.log("Game started");
+//     started = true;
+
+//     levelUp();
+//   }
+// });
 
 function btnFlash(randbtn) {
   randbtn.classList.add("flash");
@@ -44,23 +71,42 @@ function levelUp() {
   btnFlash(randBtn);
 }
 
+// and from here to
 function checkAns(idx) {
-  // let idx = level-1;
-
   if (userSeq[idx] === gameSeq[idx]) {
-    // console.log("same value");
     if (userSeq.length == gameSeq.length) {
       setTimeout(levelUp, 500);
     }
   } else {
-    h2.innerHTML = `Game over! Your score was <b> ${level} </b> <br> Press any key to Restart the game`;
-    document.querySelector("body").style.backgroundColor = "red";
-    setTimeout(function () {
-      document.querySelector("body").style.backgroundColor = "white";
+    h2.innerHTML = `Game over! Your score was <b>${level}</b><br>${
+      isMobile ? "Tap anywhere to Restart" : "Press any key to Restart"
+    }`;
+    document.body.classList.add("game-over");
+    setTimeout(() => {
+      document.body.classList.remove("game-over");
     }, 150);
     reset();
   }
 }
+// to here
+
+// function checkAns(idx) {
+//   // let idx = level-1;
+
+//   if (userSeq[idx] === gameSeq[idx]) {
+//     // console.log("same value");
+//     if (userSeq.length == gameSeq.length) {
+//       setTimeout(levelUp, 500);
+//     }
+//   } else {
+//     h2.innerHTML = `Game over! Your score was <b> ${level} </b> <br> Press any key to Restart the game`;
+//     document.querySelector("body").style.backgroundColor = "red";
+//     setTimeout(function () {
+//       document.querySelector("body").style.backgroundColor = "white";
+//     }, 150);
+//     reset();
+//   }
+// }
 
 function btnPress() {
   let btn = this;
